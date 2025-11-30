fdf65671949adf06423ac974e3b47e7c4356a102
= <=>
2025-11-30 21:35:23 +0700

    Menambahkan inisiasi pada class Hero

--- hero.py
+++ hero.py
@@ -1,17 +1,15 @@
 class Hero :
-    pass
+    def __init__(self,inputName, inputHealth, inputPower, inputArmor):
+        self.name = inputName
+        self.health = inputHealth
+        self.power = inputPower
+        self.armor = inputArmor
 
 
-hero1 = Hero()
-hero1.name = "sniper"
-hero1.health = 100
+hero1 = Hero("sniper", 100, 10, 4)
+hero2 = Hero("mirana", 100, 15, 1)
+hero3 = Hero("ucup", 1000, 100, 0)
 
-hero2 = Hero()
-hero2.name = "sven"
-hero2.heatlh = 200
-
-hero3 = Hero()
-hero3.name = "ucup"
-hero3.health = 1000
-
-print(hero1.__dict__)
+print(hero1.name)
+print(hero2.health)
+print(hero1.armor)
