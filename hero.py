a87933f943f1117a264e644f5157318329cb5887
= <=>
2025-11-30 20:58:50 +0700

    Menambahkan file baru hero.py

---
+++ hero.py
@@ -0,0 +1,17 @@
+class Hero :
+    pass
+
+
+hero1 = Hero()
+hero1.name = "sniper"
+hero1.health = 100
+
+hero2 = Hero()
+hero2.name = "sven"
+hero2.heatlh = 200
+
+hero3 = Hero()
+hero3.name = "ucup"
+hero3.health = 1000
+
+print(hero1.__dict__)
