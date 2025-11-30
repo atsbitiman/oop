d052df81c0ff8bbc1f690304c2724d19416df41c
= <=>
2025-11-30 22:21:27 +0700

    Menambahkan file method.py

---
+++ method.py
@@ -0,0 +1,24 @@
+class Hero :
+    jumlah_hero = 0
+    def __init__(self, inputName, inputHealth, inputPower, inputArmor):
+        self.name = inputName
+        self.health = inputHealth
+        self.power = inputPower
+        self.armor = inputArmor
+        Hero.jumlah_hero += 1
+    def siapa(self) :
+        print("Namaku adalah " + self.name)
+    def healthUp(self,up) :
+        self.health += up
+    def getHealth(self) :
+        return self.health
+
+
+hero1 = Hero("sniper", 100, 10, 5)
+hero2 = Hero("mario bros", 90, 5, 10)
+
+hero1.siapa()
+hero1.healthUp(10)
+
+print(hero1.health)
+print(hero1.getHealth())
