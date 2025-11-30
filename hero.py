ab0ebcbafeaf93742af44f63a6e6cd8f680642c4
= <=>
2025-11-30 21:56:41 +0700

    Memambahkan attribut class

--- hero.py
+++ hero.py
@@ -1,15 +1,17 @@
 class Hero :
+    jumlah = 0
     def __init__(self,inputName, inputHealth, inputPower, inputArmor):
         self.name = inputName
         self.health = inputHealth
         self.power = inputPower
         self.armor = inputArmor
+        Hero.jumlah += 1
+        print("Membuat objek Hero")
 
 
 hero1 = Hero("sniper", 100, 10, 4)
+print(Hero.jumlah)
 hero2 = Hero("mirana", 100, 15, 1)
+print(Hero.jumlah)
 hero3 = Hero("ucup", 1000, 100, 0)
-
-print(hero1.name)
-print(hero2.health)
-print(hero1.armor)
+print(Hero.jumlah)
